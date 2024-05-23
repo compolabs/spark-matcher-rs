5cf44327d799c34751d5859161234553460cf3fc
Paul <zhpv96@gmail.com>
2024-05-23 17:01:44 +0300

    first commit

--- src/common.rs
+++ src/common.rs
@@ -1,9 +1,7 @@
-use ::log::debug;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use reqwest::Client;
 use serde::Deserialize;
-
-use std::cmp::Ordering;
+use itertools::Itertools;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum OrderType {
@@ -28,42 +26,59 @@ pub struct IndexerOrder {
 }
 
 pub fn ev(key: &str) -> Result<String> {
-    Ok(std::env::var(key)?)
+    std::env::var(key).map_err(|e| anyhow!("Failed to get env variable {}: {}", key, e))
 }
 
-fn format_indexer_url(order_type: OrderType) -> String {
-    let order_count = ev("FETCH_ORDER_LIMIT").unwrap_or("1000".to_string());
-    let order_type_str = format!(
-        "/spot/orders?limit={}&isOpened=true&orderType={}",
-        order_count,
-        match order_type {
+struct Config {
+    indexer_url: String,
+    fetch_order_limit: String,
+}
+
+impl Config {
+    fn new() -> Result<Self> {
+        let indexer_url = ev("INDEXER_URL")?;
+        let fetch_order_limit = ev("FETCH_ORDER_LIMIT").unwrap_or_else(|_| "1000".to_string());
+        Ok(Self {
+            indexer_url,
+            fetch_order_limit,
+        })
+    }
+
+    fn format_indexer_url(&self, order_type: OrderType) -> String {
+        let order_type_str = match order_type {
             OrderType::Sell => "sell",
             OrderType::Buy => "buy",
-        }
-    );
-    let url = format!(
-        "{}{}",
-        ev("INDEXER_URL").unwrap_or("<ERROR>".to_owned()),
-        order_type_str
-    );
-    debug!("Final indexer URL is: `{}`", &url);
-    url
+        };
+        format!(
+            "{}/spot/orders?limit={}&isOpened=true&orderType={}",
+            self.indexer_url, self.fetch_order_limit, order_type_str
+        )
+    }
 }
 
 pub async fn fetch_orders_from_indexer(order_type: OrderType) -> Result<Vec<IndexerOrder>> {
-    let indexer_url = format_indexer_url(order_type);
-    let sort_func: for<'a, 'b> fn(&'a IndexerOrder, &'b IndexerOrder) -> Ordering = match order_type
-    {
-        OrderType::Buy => |a, b| b.base_price.cmp(&a.base_price),
-        OrderType::Sell => |a, b| a.base_price.cmp(&b.base_price),
-    };
-    let mut orders_deserialized = Client::new()
-        .get(indexer_url)
+    let config = Config::new()?;
+    let indexer_url = config.format_indexer_url(order_type);
+
+    let response = Client::new()
+        .get(&indexer_url)
         .send()
         .await?
-        .json::<Vec<IndexerOrder>>()
-        .await?;
-    orders_deserialized.sort_by(sort_func);
+        .error_for_status()?;
 
-    Ok(orders_deserialized)
+    let orders_deserialized: Vec<IndexerOrder> = response.json().await?;
+
+    let orders_iter = orders_deserialized.into_iter();
+
+    let orders = match order_type {
+        OrderType::Buy => {
+            orders_iter.sorted_by_key(|order| order.base_price.clone()).collect::<Vec<IndexerOrder>>()
+        }
+        OrderType::Sell => {
+            orders_iter.sorted_by_key(|order| order.base_price.clone()).rev().collect::<Vec<IndexerOrder>>()
+        }
+    };
+
+    Ok(orders)
 }
+
