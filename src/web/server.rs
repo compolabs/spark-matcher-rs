99102cdfa2302748078204eac0310a740827e547
nazgull08 <nazgull08@protonmail.com>
2024-08-07 12:06:37 +0300

    added server number, next update add it to env

--- src/web/server.rs
+++ src/web/server.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use rocket::{Build, Rocket};
+use rocket::{Build, Config, Rocket};
 use rocket_okapi::swagger_ui::make_swagger_ui;
 use sqlx::PgPool;
 
@@ -8,7 +8,10 @@ use super::routes::{get_docs, get_routes};
 use crate::management::manager::OrderManager;
 
 pub fn rocket(db_pool: PgPool, order_manager: Arc<OrderManager>) -> Rocket<Build> {
-    rocket::build()
+    let config = Config::figment()
+        .merge(("port",8001));
+
+    rocket::custom(config)
         .manage(db_pool)
         .manage(order_manager)
         .mount("/", get_routes())
