bc8bc2bdd59e7f0ebb1a829c795bf1d149cb781c
nazgull08 <nazgull08@protonmail.com>
2024-10-06 14:27:42 +0300

    hotfix

--- src/web/server.rs
+++ src/web/server.rs
@@ -8,9 +8,17 @@ use super::routes::{get_docs, get_routes};
 use crate::management::manager::OrderManager;
 
 pub fn rocket(db_pool: PgPool, order_manager: Arc<OrderManager>) -> Rocket<Build> {
-    rocket::build()
-        .manage(db_pool)
-        .manage(order_manager)
-        .mount("/", get_routes())
-        .mount("/swagger", make_swagger_ui(&get_docs()))
+    let port = crate::config::ev("ROCKET_PORT")
+        .unwrap()
+        .parse()
+        .expect("Invalid port number");
+
+    rocket::custom(rocket::Config {
+        port,
+        ..rocket::Config::default()
+    })
+    .manage(db_pool)
+    .manage(order_manager)
+    .mount("/", get_routes())
+    .mount("/swagger", make_swagger_ui(&get_docs()))
 }
